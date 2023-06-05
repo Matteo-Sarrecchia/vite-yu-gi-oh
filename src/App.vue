fc40a6c35dd9b222212371a552707b8de8adfd50
Matteo Sarrecchia <morrea@hotmail.it>
2023-06-05 16:15:06 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<template>
+  <HelloWorld />
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+</style>
